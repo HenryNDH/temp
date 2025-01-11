8080ee03e429a1ccbabfb378b52ac70a67d78672
Nguyen Duy Hoang <hoangnguyenduy7@gmail.com>
2025-01-10 19:14:13 -0500

    Initial work on express app and server

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,14 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: globals.node, // Enable Node.js globals (e.g., `process`)
+    },
+  },
   pluginJs.configs.recommended,
 ];
